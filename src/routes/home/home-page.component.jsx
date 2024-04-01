e42fa7487b937562e24cee8796ae3b61092ac4f7
Camilo Pinzon <pinzonac@gmail.com>
2024-03-31 21:46:06 -0500

    modify metas from index.html

--- src/routes/home/home-page.component.jsx
+++ src/routes/home/home-page.component.jsx
@@ -1,4 +1,4 @@
-import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Helmet } from "react-helmet-async";
 
 import Greeting from "../../components/greeting/greeting.component";
 import PersonalInfo from "../../components/personal-info/personal-info.component";
@@ -8,12 +8,15 @@ const HomePage = () => {
 	return (
 		<main>
 			<Helmet>
-				<title>Camilo Pinzon - Web Developer</title>
-				<link rel="canonical" href="https://www.camilopinzon.tech/" />
+				<title>Camilo Pinzón - Web Developer | Freelancer</title>
+				<meta
+					name="title"
+					content="Camilo Pinzón - Web Developer | Freelancer"
+				/>
 				<meta
 					name="description"
 					content="Self-taught and dedicated. A web developer with ability to assimilate work under pressure and great facility for teamwork, no matter if it's locally or remote. He brings his experience to find solutions for all the possible problems that can happen in all the project stages and brings calm to the customer."
-				></meta>
+				/>
 			</Helmet>
 			<Greeting />
 			<PersonalInfo />
